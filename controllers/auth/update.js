c0886fff62a1d959a42fad5b605ed714899ff020
Izaz Rizqullah <88955800+izazrizqullah@users.noreply.github.com>
2023-02-02 07:22:12 +0700

    update: package

--- controllers/auth/update.js
+++ controllers/auth/update.js
@@ -1,4 +1,21 @@
 const { User } = require("../../models");
 const { ROLE, VERIFIED } = require("../../utils/enum.js");
 
-module.exports = {};
+module.exports = {
+  update: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { is_verified_role } = req.body;
+
+      const user = await User.findOne({ where: { id } });
+
+      if (!user) {
+        return res.code(404).send({
+          message: "User not found",
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
