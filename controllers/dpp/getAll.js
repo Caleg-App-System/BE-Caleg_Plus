4a4d068d329711246e9202ad2bbe05043c6b1575
Izaz Rizqullah <88955800+izazrizqullah@users.noreply.github.com>
2023-03-29 05:54:08 +0700

    update: test get all dpp

--- controllers/dpp/getAll.js
+++ controllers/dpp/getAll.js
@@ -2,20 +2,24 @@ const { Dpp } = require("../../models");
 
 module.exports = {
   getAll: async (request, reply) => {
-    const find = await Dpp.findAll();
+    try {
+      const find = await Dpp.findAll();
 
-    if (!find) {
-      return reply.code(404).send({
-        status: false,
-        message: "data not found",
-        data: null,
+      if (!find) {
+        return reply.code(404).send({
+          status: false,
+          message: "data not found",
+          data: null,
+        });
+      }
+
+      reply.code(200).send({
+        status: true,
+        message: "berhasil mengambil data",
+        data: find,
       });
+    } catch (err) {
+      console.log(err);
     }
-
-    reply.code(200).send({
-      status: true,
-      message: "berhasil mengambil data",
-      data: find,
-    });
   },
 };
